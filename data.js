0d88f32d4d8f4b5beb343087ab30bed23b1acfc5
truyenhaymoingay2024 <b480ca2e3974c0f19098c50cf0fe86b9@protonmail.com>
2026-01-14 05:10:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -495,15 +495,17 @@ const LIBRARY = [
     {        
         id: 32,
         folderName: "ManThienHoaVu",
-        title: "Mạn Thiên Hoa Vũ (đang update 40/87)",
+        title: "Mạn Thiên Hoa Vũ (đang update 52/87)",
         author: "Thường Yên",
         cover: "https://github.com/truyenhaymoingay2024/sourceTruyen/raw/main/ManThienHoaVu/cover.jpg",
         desc: 'Mạn Thiên Hoa Vũ được lấy bối cảnh dưới thời vua Trần Anh Tông, vị hoàng đế thứ tư của Hoàng triều Trần trong lịch sử Việt Nam.',
 	    chapters: 87,
         tracks: [
-            ...batch(1, 40, 5, 'm4a'),
+            ...batch(1, 50, 5, 'm4a'),
+            manual("Chương 51 - 52 (Hoàn quyển 1)", "c51-52.m4a")
         ]
     },
 
 ];
+
 
