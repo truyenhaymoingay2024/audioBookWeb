f35041ff5cad3c9c2b414614f6d536e82a5eb8e7
truyenhaymoingay2024 <b480ca2e3974c0f19098c50cf0fe86b9@protonmail.com>
2026-03-17 21:16:48 +0700

    update data

--- data.js
+++ data.js
@@ -878,14 +878,14 @@ const LIBRARY = [
     {        
         id: 55,
         folderName: "MonQua",
-        title: "Món Quà [đang update 70/118]",
+        title: "Món Quà [đang update 90/118]",
         author: "Khâm Điểm Phế Sài",
         cover: "https://github.com/truyenhaymoingay2024/sourceTruyen/raw/main/MonQua/cover.png",
         desc: 'Anh là ác quỷ, và cũng là món quà.\nTin tốt: Không ai biết cô thầm yêu anh trai mình.\nTin xấu: Anh trai cô là đồ cà chớn.\n“Con trai có em gái sẽ không lắp giỏ đựng đồ ở yên sau xe đạp.”\n\nNote: Nam nữ chính không cùng huyết thống và hộ khẩu.\nTóm tắt một câu: Cặp anh em “hờ” yêu nhau lắm cắn nhau đau.\nChủ đề: Tình yêu sâu đậm nảy mầm từ thuở thiếu thời.',
 	    isH: true,
         chapters: 118,
         tracks: [
-            ...batch(1, 70, 5, 'm4a'),
+            ...batch(1, 90, 5, 'm4a'),
         ]
     },
 ];
