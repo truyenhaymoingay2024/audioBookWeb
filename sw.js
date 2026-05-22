0a0932228e11be77ba667003d4c2b5b149cccdb5
truyenhaymoingay2024 <b480ca2e3974c0f19098c50cf0fe86b9@protonmail.com>
2026-05-22 21:34:00 +0700

    update speed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tungu-audio-v2026.05.09'; 
+const CACHE_NAME = 'tungu-audio-v2026.05.22'; 
 const ASSETS_TO_CACHE =[
     './',
     './index.html',
@@ -14,7 +14,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('Đã mở cache v2026.05.09');
+                console.log('Đã mở cache v2026.05.22');
                 return cache.addAll(ASSETS_TO_CACHE);
             })
     );
